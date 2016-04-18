192ecbea9036c4bce15e61c8f05bd36db75d10e0
geommer <geommer@gmail.com>
2016-04-18 18:01:40 +0200

    Fix potential segfault for strncpy

--- src/ya_draw.c
+++ src/ya_draw.c
@@ -356,11 +356,9 @@ inline void ya_get_cur_window_title(ya_block_t * blk) {
 		wm_ck = xcb_ewmh_get_wm_name(ya.ewmh, ya.curwin);
 		wmvis_ck = xcb_ewmh_get_wm_visible_name(ya.ewmh, ya.curwin);
 		if (xcb_ewmh_get_wm_name_reply(ya.ewmh, wm_ck, &reply, NULL) == 1 || xcb_ewmh_get_wm_visible_name_reply(ya.ewmh, wmvis_ck, &reply, NULL) == 1) {
-			if(reply.strings_len < blk->bufsize)
-				strncpy(blk->buf, reply.strings, reply.strings_len);
-			else 
-				strncpy(blk->buf, reply.strings, blk->bufsize);
-			blk->buf[reply.strings_len]='\0';
+			int len = blk->bufsize < reply.strings_len ? blk->bufsize : reply.strings_len;
+			strncpy(blk->buf, reply.strings, len);
+			blk->buf[len]='\0';
 		}
 		xcb_ewmh_get_utf8_strings_reply_wipe(&reply);
 	}
